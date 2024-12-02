b4de2e5f4d444e55875a12e49be1c90597083388
RaiMaru24 <alenbijuk2003@gmail.com>
2024-12-02 13:46:58 +0530

    Update script

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Meraki Kernel by Niranjan
+kernel.string=Vertigo Kernel by Alen
 do.devicecheck=1
 do.modules=0
 do.systemless=1
